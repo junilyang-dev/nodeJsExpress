9b3f0368cd22e2e705c0d4693a5b5567e7e51fec
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-04-13 11:39:27 +0000

    0.3 Frontend Setup

--- nodemon.js
+++
@@ -1,9 +0,0 @@
-// {
-//   "exec":"babel-node src/server.js"
-// }
-module.exports = {
-  // src/public 디렉토리 내 파일 변화는 무시됩니다.
-  ignore:["src/public/*"],
-  // exec 명령은 babel-node를 사용하여 src/server.js를 실행합니다.
-  exec:"babel-node src/server.js"
-};
--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
+import http from 'http';
+import webSocket from 'ws';
 import express from "express";
 
 const app = express();
 
+//console.log("hello");
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req,res) => res.render("home"));
+app.get("/*", (req,res) => res.redirect("/"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`)
+//app.listen(3000,handleListen);
+//#npm run dev 
+//shell 에서 실행하면 hello 콘솔 확인
+//#1.2
+const server = http.createServer(app);
+const wss = new webSocket.Server({server});
 
-const handleListen = () => console.log("Listening on port 3000");
+//#1.3
+function handleConnection(socket){
+  console.log(socket);
+}
 
-app.listen(3000, handleListen);
+wss.on("connection",handleConnection);
+
+server.listen(3000,handleListen);
