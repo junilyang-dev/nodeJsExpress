cb2687bfa1b2be866a82855d7091245644000eb8
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-04-16 12:19:43 +0000

    add nick form and send JSON.stringify

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,12 @@
 const socket = new WebSocket(`wss://${window.location.host}`);
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nick');
+
+function makeMessage(type, payload) {
+  const msg = {type, payload};
+  return JSON.stringify(msg); 
+}
 
 function handleOpen() {
   console.log("Connected to the Server ✅");
@@ -27,9 +33,16 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector('input');
-  socket.send(input.value);
+  socket.send(makeMessage("new_message",input.value));
   console.log(input.value);
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage("nickname",input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
